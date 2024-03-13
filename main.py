e40029c6df24c7f2f13992940e71cb266ff90742
smarinak <sladkovamk@yandex.ru>
2024-03-13 16:50:53 +0300

    добавлена функция смены директории, модифицированы условия основного цикла

--- main.py
+++ main.py
@@ -5,28 +5,41 @@ work_dir = settings.working_directory
 current_dir = settings.working_directory
 
 
-def make_dir(*dir_names):
-    if not command_key:
-        print('Вы не передали параметры')
-    else:
-        try:
-            for name in dir_names:
-                os.mkdir(os.path.join(current_dir, name))
-        except FileExistsError:
-            print(f'Папка с именем "{name}" уже существует')
+def mkdir(*dir_names):
+    try:
+        for name in dir_names:
+            os.mkdir(os.path.join(current_dir, name))
+    except FileExistsError:
+        print(f'Папка с именем "{name}" уже существует')
 
 
-def delete_dir(*dir_names):
-    if not command_key:
-        print('Вы не передали параметры')
+def rm(*dir_names):
+    try:
+        for name in dir_names:
+            os.rmdir(os.path.join(current_dir, name))
+    except FileNotFoundError:
+        print(f'Папки с именем "{name}" не существует внутри текущей директории')
+    except OSError:
+        print('Ошибка удаления папки')  # пояснить!!!
+
+
+def cd(*dir_name):
+    global current_dir
+    if len(dir_name) > 1:
+        print('Вам следует передать только 1 параметр')
     else:
-        try:
-            for name in dir_names:
-                os.rmdir(os.path.join(current_dir, name))
-        except FileNotFoundError:
-            print(f'Папки с именем "{name}" не существует внутри текущей директории')
-        except OSError:
-            print('Ошибка удаления папки')
+        if dir_name[0] == '..':
+            if current_dir != work_dir:
+                current_dir = os.path.dirname(os.path.join(dir_name[0], current_dir))
+            else:
+                print('Нельзя подняться выше корневой папки')
+        else:
+            new_dir = os.path.join(current_dir, dir_name[0])
+            if os.path.isdir(new_dir):
+                current_dir = new_dir
+            else:
+                print('Такой папки не существует внутри текущей')
+
 
 
 while True:
@@ -35,8 +48,11 @@ while True:
     try:
         command_name = command[0]
         command_key = command[1:]
-        if command_name in ['make_dir', 'delete_dir']:
-            globals()[command_name](*command_key)
+        if command_name in ['mkdir', 'rm', 'cd']:
+            if command_key:
+                globals()[command_name](*command_key)
+            else:
+                print('Вы не передали параметры')
         else:
             print('Такой команды не существует')
     except IndexError:
