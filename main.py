c7a12cd9569a47afc6508ef85fc24fbcba4a8376
smarinak <sladkovamk@yandex.ru>
2024-03-14 17:16:55 +0300

    добавлены функции записи и чтения файлов

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def cd(*dir_name):
     else:
         if dir_name[0] == '..':
             if current_dir != work_dir:
-                current_dir = os.path.dirname(os.path.join(dir_name[0], current_dir))
+                current_dir = os.path.dirname(os.path.join(dir_name[0], current_dir))  # пояснить!!!
             else:
                 print('Нельзя подняться выше корневой папки')
         else:
@@ -50,15 +50,45 @@ def mkfile(*file_names):
         print(f'Файл с именем "{name}" уже существует')
 
 
+def write(*params):
+    file_name = params[0]
+    text = ' '.join(params[1:])
+    try:
+        file_path = os.path.join(current_dir, file_name)
+        with open(file_path, 'w') as file:
+            file.write(text)
+    except IsADirectoryError:
+        print(f'"{file_name[0]}" является директорией')
+    except PermissionError:
+        print(f'Доступ для записи в файл "{file_name}" запрещен')
+
+
+def read(*file_name):
+    if len(file_name) > 1:
+        print('Вам следует передать только 1 параметр')
+    else:
+        try:
+            file_path = os.path.join(current_dir, file_name[0])
+            with open(file_path, 'r') as file:
+                print(file.read())
+        except FileNotFoundError:
+            print(f'Файла "{file_name[0]}" не существует внутри текущей папки')
+        except IsADirectoryError:
+            print(f'"{file_name[0]}" является директорией')
+        except PermissionError:
+            print(f'Доступ для чтения файла "{file_name[0]}" запрещен')
+
+
+
 while True:
     print(f'{current_dir}: ', end='')
     command = input().split()
     try:
         command_name = command[0]
         command_key = command[1:]
-        if command_name in ['mkdir', 'rm', 'cd', 'mkfile']:
+        if command_name in ['mkdir', 'rm', 'cd', 'mkfile', 'write', 'read']:
             if command_key:
-                globals()[command_name](*command_key)
+                globals()[command_name](*command_key)  # пояснить!!!
             else:
                 print('Вы не передали параметры')
         else:
